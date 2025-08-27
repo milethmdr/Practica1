485e15728a7e19bc152c57f7ea3db26ebeffcf05
milethmdr <milethmdr06@gmailcom>
2025-08-26 22:44:01 -0600

    Agrrgue archivo

--- index.js
+++ index.js
@@ -6,8 +6,37 @@ const rd = readline.createInterface({
     output: stdout
 });
 
-rd.question('what you name?', (name)=>{
-    console.log("Welcome" + name)
-    rd.close()
+// rd.question('what you name?', (name)=>{
+//     console.log("Welcome" + name)
+//     rd.close()
+// });
+
+// let word = "Hola esto es JavaScript";
+// //ToLowerCase
+// console.log(word.toLocaleLowerCase())
+// console.log(word.toLocaleUpperCase())
+// console.log(word.trim().split(/\s+/))
+// console.log("reverse:"+word.split("").reverse().join(''))
+// console.log(word.replaceAll("","").split(','))
+
+//Verificar si la palabra es palindromo
+//Escribir la palabra en consola y procesar para saber si lo es
+
+rd.question('Ingrese la palabra para saber si es palindromo:', (word)=>{
+    let palabraInvertida = word.split('').reverse().join(" ");
+    //debug
+    console.log(palabraInvertida + " " + word)
+    let respuesta;
+    //  if(word == palabraInvertida){
+    //     respuesta = "¿Es palindromo?"
+    //  }else{
+    //     respuesta = "No es palindromo"
+    //  }
+
+    //usando ternario
+    respuesta= word == palabraInvertida? "Es palindromo" : "No es palindromo"
+     console.log(word + " : " + respuesta)
+     rd.close()
 });
+
 
